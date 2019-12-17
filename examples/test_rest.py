33dd970096bb1b4a7c90cd2ad6773b097404d368
bluesky0125 <time2go@yeah.net>
2019-12-17 14:09:40 +0800

    add test images

--- examples/test_rest.py
+++ examples/test_rest.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-db = "/media/ubuntu/Investigation/DataSet/Image/Face/db/"
+db = "data/"
 
 def test_face_match():
     url = 'http://127.0.0.1:5000/face_match'
     # open file in binary mode
-    files = {'file1': open(db+'3.jpg', 'rb'),
-             'file2': open(db+'4.jpg', 'rb')}     
+    files = {'file1': open(db+'1.jpg', 'rb'),
+             'file2': open(db+'2.jpeg', 'rb')}     
     resp = requests.post(url, files=files)
     print( 'sim:\n', json.dumps(resp.json()) )
     
