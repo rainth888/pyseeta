d2428f424eee52c951e0f2cb4964ce96ec051445
bluesky0125 <time2go@yeah.net>
2019-12-17 11:26:38 +0800

    update

--- examples/test_rest.py
+++ examples/test_rest.py
@@ -9,7 +9,7 @@ def test_face_match():
     files = {'file1': open(db+'3.jpg', 'rb'),
              'file2': open(db+'4.jpg', 'rb')}     
     resp = requests.post(url, files=files)
-    print( 'face_match response:\n', json.dumps(resp.json()) )
+    print( 'sim:\n', json.dumps(resp.json()) )
     
 if __name__ == '__main__':
     test_face_match()
